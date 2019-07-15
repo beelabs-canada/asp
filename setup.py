ea763c2c9bce476a74804fdfa1aff11e8d65ec5d
Mario Bonito <mario@beelabs.ca>
2019-07-15 10:13:28 -0400

    initial package setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='Chipi',
+    version='0.1dev',
+    packages=['chipi',],
+    license=open('LICENSE').read(),
+    long_description=open('README.md').read(),
+)
